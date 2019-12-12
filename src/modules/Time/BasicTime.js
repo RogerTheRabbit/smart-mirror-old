42db21c764d013dca93833a27dda1cf018058cd1
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2019-12-11 22:57:59 -0500

    Refactored some code in BasicTime

--- src/modules/Time/BasicTime.js
+++ src/modules/Time/BasicTime.js
@@ -3,26 +3,9 @@ import React, {useEffect} from 'react'
 import './BasicTime.css'
 
 export default function BasicTime() {
-    
-    function updateClock() {
-        var date = document.getElementById('date')
-        date.innerText = getFormattedDate();
-        var time = document.getElementById('time')
-        time.innerText = getFormattedTime();
-    }
 
-    function getFormattedDate() {
-        var date = new Date();
-        return date.toDateString().toUpperCase()
-    }
-
-    function getFormattedTime() {
-        var date = new Date();
-        return date.getHours().pad('0', 2) + ":" + date.getMinutes().pad('0', 2)+":"+date.getSeconds().pad('0', 2);
-    }
-
-    Number.prototype.pad = function(padWith, len) {
-        var str = this + ''
+    function pad(num, padWith, len) {
+        var str = num + '' // Convert number into string
         while (str.length < len) {
             str = padWith + str;
         }
@@ -30,6 +13,24 @@ export default function BasicTime() {
     }
 
     useEffect(() => {
+        
+        function getFormattedDate() {
+            var date = new Date();
+            return date.toDateString().toUpperCase()
+        }
+    
+        function getFormattedTime() {
+            var date = new Date();
+            return pad(date.getHours(), '0', 2) + ":" + pad(date.getMinutes(), '0', 2)+":"+pad(date.getSeconds(),'0', 2);
+        }
+
+        function updateClock() {
+            var date = document.getElementById('date')
+            date.innerText = getFormattedDate();
+            var time = document.getElementById('time')
+            time.innerText = getFormattedTime();
+        }
+
         updateClock()
         const interval = setInterval(() => {
             updateClock()
