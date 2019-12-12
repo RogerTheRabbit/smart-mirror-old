cde0bb580680066960abfe1b48fbadd49bda35a9
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2019-12-12 02:22:37 -0500

    Quote now changes every hour

--- src/modules/InspirationalQuote/InspirationalQuote.js
+++ src/modules/InspirationalQuote/InspirationalQuote.js
@@ -2,29 +2,35 @@ import React, {useEffect, useState} from 'react'
 import './InspirationalQuote.css'
 
 const QUOTES_FEED = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fquotes%2F.rss'; //'https://www.reddit.com/r/quotes/.rss'
+const updateInterval = 3600000
 
 function InspirationalQuote() {
 
     const [quote, setQuote] = useState('...')
-    
+
+    function FetchDataFromRssFeed() {
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+                var myObj = JSON.parse(request.responseText);
+                console.log(myObj)
+                setQuote(myObj.items[0].title.replace(/&quot;/g, '"'))
+            }
+        }
+        request.open("GET", QUOTES_FEED, true);
+        request.send();
+    }
+
     useEffect(() => {
 
-        function FetchDataFromRssFeed() {
-            var request = new XMLHttpRequest();
-            request.onreadystatechange = () => {
-                if (request.readyState === 4 && request.status === 200) {
-                    var myObj = JSON.parse(request.responseText);
-                    console.log(myObj)
-                    setQuote(unescape(myObj.items[1].title))
-                }
-            }
-            request.open("GET", QUOTES_FEED, true);
-            request.send();
-        }
-
         FetchDataFromRssFeed()
+        const interval = setInterval(() => {
+            FetchDataFromRssFeed()
+        }, updateInterval)
+        
+        return () => clearInterval(interval);
 
-    }, [setQuote])
+    }, [])
 
     return (
         <div className="quote">
