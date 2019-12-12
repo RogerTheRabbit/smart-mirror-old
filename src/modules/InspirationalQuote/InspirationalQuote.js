127dac52b01afe6b20f0d87bc7d447ef22e1f729
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2019-12-12 01:54:53 -0500

    Added Inspiriational quote module

--- src/modules/InspirationalQuote/InspirationalQuote.js
+++ src/modules/InspirationalQuote/InspirationalQuote.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import './InspirationalQuote.css'
+
+const QUOTES_FEED = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.reddit.com%2Fr%2Fquotes%2F.rss'; //'https://www.reddit.com/r/quotes/.rss'
 
 function InspirationalQuote() {
+
+    const [quote, setQuote] = useState('...')
+    
+    useEffect(() => {
+
+        function FetchDataFromRssFeed() {
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    var myObj = JSON.parse(request.responseText);
+                    console.log(myObj)
+                    setQuote(unescape(myObj.items[1].title))
+                }
+            }
+            request.open("GET", QUOTES_FEED, true);
+            request.send();
+        }
+
+        FetchDataFromRssFeed()
+
+    }, [setQuote])
+
     return (
-        <div>
-            
+        <div className="quote">
+            <h1>{quote}</h1>
         </div>
     )
 }
